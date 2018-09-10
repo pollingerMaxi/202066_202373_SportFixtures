fca5d562288dfe7c8c8efee4ac9f4c0a727056d6
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-10 20:09:15 -0300

    Cambios para que lo vea Javier

--- SportFixtures/SportFixtures.Data.Access/Context.cs
+++ SportFixtures/SportFixtures.Data.Access/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SportFixtures.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,11 @@ namespace SportFixtures.Data.Access
 {
     public class Context : DbContext
     {
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=SportFixturesTest;Trusted_Connection=True;Integrated Security=True");
+        }
     }
 }
