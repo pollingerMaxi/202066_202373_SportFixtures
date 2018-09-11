5b5c1e9d17c31e5d00754b00cf27c178f9859895
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-10 21:12:51 -0300

    Agrego TestCleanUp y nuevo test

--- SportFixtures/SportFixtures.Test/DataTests/TeamDataTests.cs
+++ SportFixtures/SportFixtures.Test/DataTests/TeamDataTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportFixtures.Data.Access;
+using SportFixtures.Data.Entities;
 using SportFixtures.Data.Repository;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,18 @@ namespace SportFixtures.Test.DataTests
     [TestClass]
     public class TeamDataTests
     {
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            using (var context = new Context())
+            {
+                var unitOfWork = new UnitOfWork(context);
+                var teams = unitOfWork.TeamRepository.Get();
+                context.RemoveRange(teams);
+                context.SaveChanges();
+            }
+        }
+
         [TestMethod]
         public void GetAllTeamsWithNoTeamsInRepository()
         {
@@ -18,6 +31,17 @@ namespace SportFixtures.Test.DataTests
             var unitOfWork = new UnitOfWork(context);
             var teams = unitOfWork.TeamRepository.Get();
             Assert.IsTrue(teams.Count() == 0);
+        }
+
+        [TestMethod]
+        public void GetAllTeamsWithOneTeamInRepository()
+        {
+            var context = new Context();
+            var unitOfWork = new UnitOfWork(context);
+            unitOfWork.TeamRepository.Insert(new Team());
+            context.SaveChanges();
+            var teams = unitOfWork.TeamRepository.Get();
+            Assert.IsTrue(teams.Count() == 1);
         }
     }
 }
