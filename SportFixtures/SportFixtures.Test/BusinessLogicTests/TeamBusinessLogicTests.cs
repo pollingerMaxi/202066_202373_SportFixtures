04f075ae6019be68aef0c055a1fd0c0da975b118
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-11 18:26:48 -0300

    Agrego clase para probar logica de negocio de Team

---
+++ SportFixtures/SportFixtures.Test/BusinessLogicTests/TeamBusinessLogicTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.Test.BusinessLogicTests
+{
+    [TestClass]
+    public class TeamBusinessLogicTests
+    {
+    }
+}
