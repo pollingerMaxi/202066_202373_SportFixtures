81a7ad5ffa5c96242b7918a955c04759bacc3594
Javier Aramberri <javieraramberri37@gmail.com>
2018-09-19 20:24:31 -0300

    Agrego exception nombre Sport invalido

---
+++ SportFixtures/SportFixtures.Exceptions/SportExceptions/InvalidSportNameException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.Exceptions.SportExceptions
+{
+    public class InvalidSportNameException : SportException
+    {
+        public InvalidSportNameException() : base("Sport name can't be empty.")
+        {
+        }
+        public InvalidSportNameException(string message) : base(message)
+        {
+        }
+    }
+}
