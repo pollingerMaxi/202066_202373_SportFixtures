fd803142d30ec2fc14df0fcb3aedb880051f6725
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-26 17:10:47 -0300

    Agrego datos al usuario

--- SportFixtures/SportFixtures.Data/Entities/User.cs
+++ SportFixtures/SportFixtures.Data/Entities/User.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
--- SportFixtures/SportFixtures.Test/BusinessLogicTests/UserBusinessLogicTests.cs
+++ SportFixtures/SportFixtures.Test/BusinessLogicTests/UserBusinessLogicTests.cs
@@ -25,5 +25,31 @@ namespace SportFixtures.Test.BusinessLogicTests
             mockRepo.Verify(x => x.Insert(It.IsAny<User>()), Times.Once());
             mockRepo.Verify(x => x.Save(), Times.Once());
         }
+
+        [TestMethod]
+        public void AddUserWithUsernameTest()
+        {
+            var username = "UserNickname";
+            var list = new List<User>();
+            var mockRepo = new Mock<IRepository<User>>();
+            mockRepo.Setup(x => x.Insert(It.IsAny<User>())).Callback<User>(x => list.Add(new User() { Username = username }));
+            IUserBusinessLogic userBL = new UserBusinessLogic(mockRepo.Object);
+            userBL.AddUser(new User() { Username = username });
+            mockRepo.Verify(x => x.Insert(It.IsAny<User>()), Times.Once());
+            mockRepo.Verify(x => x.Save(), Times.Once());
+        }
+
+        [TestMethod]
+        public void AddUserWithFullInfoOkTest()
+        {
+            var user = new User() { Name = "name", Username = "username", LastName = "lastname", Password = "hash", Email = "email@email.com" };
+            var list = new List<User>();
+            var mockRepo = new Mock<IRepository<User>>();
+            mockRepo.Setup(x => x.Insert(It.IsAny<User>())).Callback<User>(x => list.Add(user));
+            IUserBusinessLogic userBL = new UserBusinessLogic(mockRepo.Object);
+            userBL.AddUser(user);
+            mockRepo.Verify(x => x.Insert(It.IsAny<User>()), Times.Once());
+            mockRepo.Verify(x => x.Save(), Times.Once());
+        }
     }
 }
