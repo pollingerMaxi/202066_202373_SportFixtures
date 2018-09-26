41ac694b21db34708acf07008bfc8bfec9a7d303
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-26 17:05:48 -0300

    Agrego businessLogic.

---
+++ SportFixtures/SportFixtures.BusinessLogic/Implementations/UserBusinessLogic.cs
@@ -0,0 +1,25 @@
+﻿using SportFixtures.BusinessLogic.Interfaces;
+using SportFixtures.Data.Entities;
+using SportFixtures.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.BusinessLogic.Implementations
+{
+    public class UserBusinessLogic : IUserBusinessLogic
+    {
+        private IRepository<User> repository;
+
+        public UserBusinessLogic(IRepository<User> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void AddUser(User user)
+        {
+            repository.Insert(user);
+            repository.Save();
+        }
+    }
+}
---
+++ SportFixtures/SportFixtures.BusinessLogic/Interfaces/IUserBusinessLogic.cs
@@ -0,0 +1,12 @@
+﻿using SportFixtures.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.BusinessLogic.Interfaces
+{
+    public interface IUserBusinessLogic
+    {
+        void AddUser(User user);
+    }
+}
