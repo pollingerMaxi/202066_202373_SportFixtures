69ed67917fc0e165efebf049ab9b979296881e0d
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-09-30 19:28:37 -0300

    Cambio de versiones de netcore a 2.1.1, para asi todos los proyectos estan en la misma y no da problemas de versiones (downgrade). Arego migration para actualizar la base de datos con todas las entities.

---
+++ SportFixtures/SportFixtures.Data.Access/Migrations/20180930222632_Update with all entities.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportFixtures.Data.Access.Migrations
+{
+    public partial class Updatewithallentities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SportId",
+                table: "Teams",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Teams",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Encounters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    SportId = table.Column<int>(nullable: false),
+                    Team1Id = table.Column<int>(nullable: true),
+                    Team2Id = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Encounters_Teams_Team1Id",
+                        column: x => x.Team1Id,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Encounters_Teams_Team2Id",
+                        column: x => x.Team2Id,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Role = table.Column<int>(nullable: false),
+                    Token = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    EncounterId = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Encounters_EncounterId",
+                        column: x => x.EncounterId,
+                        principalTable: "Encounters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_SportId",
+                table: "Teams",
+                column: "SportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_UserId",
+                table: "Teams",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_EncounterId",
+                table: "Comments",
+                column: "EncounterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encounters_Team1Id",
+                table: "Encounters",
+                column: "Team1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encounters_Team2Id",
+                table: "Encounters",
+                column: "Team2Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Sports_SportId",
+                table: "Teams",
+                column: "SportId",
+                principalTable: "Sports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Users_UserId",
+                table: "Teams",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teams_Sports_SportId",
+                table: "Teams");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teams_Users_UserId",
+                table: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Sports");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Encounters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teams_SportId",
+                table: "Teams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teams_UserId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "SportId",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Teams");
+        }
+    }
+}
