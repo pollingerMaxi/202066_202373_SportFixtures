482f61c81f65500e5ea14c74772bff29a8f32052
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-10-01 01:09:50 -0300

    Creacion de primer controller con placeholders para arrancar a desarrollar la API. Borro controller por defecto que trae el proyecto.

---
+++ SportFixtures/SportFixtures.Portal/Controllers/TeamsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportFixtures.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportFixtures.Portal.Controllers
+{
+    [Route("api/teams")]
+    public class TeamsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ICollection<Team>> GetAllTeams()
+        {
+            return new List<Team>() { new Team(), new Team() };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Team> GetTeam(int id)
+        {
+            return new Team() { Id = id };
+        }
+
+        [HttpPost]
+        public void CreateTeam([FromBody]Team team)
+        {
+            //TeamBL.Add(team);
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateTeam(int id, [FromBody]Team team)
+        {
+            //TeamBL.Update(team);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteTeam(int id)
+        {
+            //TeamBL.Delete(new Team() { Id = id });
+        }
+    }
+}
