d4146b51b380c53aad272e24ed22cde24f6e24ad
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-10-02 20:31:07 -0300

    Agrego primer apporach del controller de login

---
+++ SportFixtures/SportFixtures.Portal/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportFixtures.BusinessLogic.Interfaces;
+using SportFixtures.Data.Entities;
+using SportFixtures.Exceptions.UserExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportFixtures.Portal.Controllers
+{
+    [Route("api/login")]
+    public class LoginController : ControllerBase
+    {
+        private IUserBusinessLogic userBusinessLogic;
+
+        public LoginController(IUserBusinessLogic userBL)
+        {
+            userBusinessLogic = userBL;
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromBody]User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                userBusinessLogic.Login(user);
+                return Ok();
+            }
+            catch (UserDoesNotExistException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (EmailOrPasswordException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
