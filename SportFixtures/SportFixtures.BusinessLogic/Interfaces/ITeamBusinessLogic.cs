ffb7029ededbd20ef7408adaf2780b50647a67f3
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-10-10 17:55:25 -0300

    Agrego documentacion

--- SportFixtures/SportFixtures.BusinessLogic/Interfaces/ITeamBusinessLogic.cs
+++ SportFixtures/SportFixtures.BusinessLogic/Interfaces/ITeamBusinessLogic.cs
@@ -6,11 +6,41 @@ namespace SportFixtures.BusinessLogic.Interfaces
 {
     public interface ITeamBusinessLogic
     {
+        /// <summary>
+        /// Adds a team to the system.
+        /// </summary>
+        /// <param name="team"></param>
         void Add(Team team);
+
+        /// <summary>
+        /// Updates a team.
+        /// </summary>
+        /// <param name="team"></param>
         void Update(Team team);
+        
+        /// <summary>
+        /// Deletes a team.
+        /// </summary>
+        /// <param name="id"></param>
         void Delete(int id);
+
+        /// <summary>
+        /// Returns exception if team does not exist.
+        /// </summary>
+        /// <param name="teamId"></param>
         void CheckIfExists(int teamId);
+
+        /// <summary>
+        /// Returns all teams in repository.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<Team> GetAll();
+
+        /// <summary>
+        /// Returns the team with given ID.
+        /// </summary>
+        /// <param name="teamId"></param>
+        /// <returns></returns>
         Team GetById(int teamId);
     }
 }
