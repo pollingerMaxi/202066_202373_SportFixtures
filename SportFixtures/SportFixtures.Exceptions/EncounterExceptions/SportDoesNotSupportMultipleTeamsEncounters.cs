387c269105a5ebbdec62f1a37a40095d14721d46
Javier Aramberri <javieraramberri37@gmail.com>
2018-10-20 14:20:28 -0300

    Agrego excepcion para deportes que solo aceptan dos equipos por encuentro

---
+++ SportFixtures/SportFixtures.Exceptions/EncounterExceptions/SportDoesNotSupportMultipleTeamsEncounters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SportFixtures.Exceptions.EncounterExceptions
+{
+    [Serializable]
+    public class SportDoesNotSupportMultipleTeamsEncounters : EncounterException
+    {
+        public SportDoesNotSupportMultipleTeamsEncounters() : base("Sport does not support encounters with multiple teams.")
+        {
+        }
+
+        public SportDoesNotSupportMultipleTeamsEncounters(string message) : base(message)
+        {
+        }
+    }
+}
