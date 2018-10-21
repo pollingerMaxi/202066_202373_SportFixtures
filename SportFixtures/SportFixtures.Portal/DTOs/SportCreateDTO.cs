54c3121e7fd56e2f6ec43b90465c5d8657bdc45b
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-10-20 22:17:11 -0300

    Agrego otro DTO para que al crear un sport no le tengas que pasar teams vacio.

---
+++ SportFixtures/SportFixtures.Portal/DTOs/SportCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportFixtures.Portal.DTOs
+{
+    public class SportCreateDTO
+    {
+        public string Name { get; set; }
+    }
+}
