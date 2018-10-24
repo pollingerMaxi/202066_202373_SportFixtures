b712e3c7314517290976eed92ae775ca8cae1eae
Javier Aramberri <javieraramberri37@gmail.com>
2018-10-24 00:00:55 -0300

    Agrego entidad Position pensando en posible implementacion

---
+++ SportFixtures/SportFixtures.Data/Entities/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SportFixtures.Data.Entities
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public int sportId { get; set; }
+        public Team Team { get; set; }
+        public int points { get; set; }
+    }
+}
