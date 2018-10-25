dae2cdb31432ec0bde12b1e21b30be1140f02ddc
Javier Aramberri <javieraramberri37@gmail.com>
2018-10-25 00:13:21 -0300

    Agrego entidad Position

---
+++ SportFixtures/SportFixtures.Data/Entities/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.Data.Entities
+{
+    public class Position
+    {
+        Team Team { get; set; }
+        int Points { get; set; }
+    }
+}
