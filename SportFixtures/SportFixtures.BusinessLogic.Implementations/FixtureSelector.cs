c57a04657286b73af71e989fba9ac44c2e5d5977
Javier Aramberri <javieraramberri37@gmail.com>
2018-11-14 19:55:24 -0300

    Creo clase e interfaz en BusinessLogic

---
+++ SportFixtures/SportFixtures.BusinessLogic.Implementations/FixtureSelector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SportFixtures.BusinessLogic.Interfaces;
+
+namespace SportFixtures.BusinessLogic.Implementations
+{
+    public class FixtureSelector:IFixtureSelector
+    {
+    }
+}
---
+++ SportFixtures/SportFixtures.BusinessLogic/Interfaces/IFixtureSelector.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportFixtures.BusinessLogic.Interfaces
+{
+    public class IFixtureSelector
+    {
+    }
+}
