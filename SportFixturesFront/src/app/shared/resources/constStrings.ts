628d30f137ae08a0e00f708e311604a1447f684b
Maximiliano Pollinger <pollinger.maxi@outlook.com>
2018-11-17 20:00:55 -0300

    Modifico la navbar

--- SportFixturesFront/src/app/shared/resources/constStrings.ts
+++ SportFixturesFront/src/app/shared/resources/constStrings.ts
@@ -1,2 +1,8 @@
 export const PROFILE: string = "Profile";
 export const USERS_MANAGEMENT: string = "Users Management";
+export const TEAMS_MANAGEMENT: string = "Teams Management";
+export const SPORTS_MANAGEMENT: string = "Sports Management";
+export const ENCOUNTERS_MANAGEMENT: string = "Encounters Management";
+export const HOME: string = "Home";
+export const LOGIN: string = "Login";
+export const LOGOUT: string = "Logout";
